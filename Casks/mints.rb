a4f906fad2c5579f771b14134430f838e9d2ff8c
Evans Thompson <evansthompson@gmail.com>
2023-11-05 18:03:40 -0600

    Added mints

---
+++ Casks/mints.rb
@@ -0,0 +1,11 @@
+cask "mints" do
+  version "1.9"
+  sha256 "04128350244c8a0cb1ce139c9367a57fb9de495c196b74b1e30970d6033d3f69"
+
+  url "https://eclecticlightdotcom.files.wordpress.com/2022/08/mints#{version}.zip"
+  name "mints"
+  desc "collection of custom log browsers and tools"
+  homepage "https://eclecticlight.co/mints-a-multifunction-utility/"
+
+  app "Mints.app"
+end
